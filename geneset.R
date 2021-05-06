035fc66ce197c0b9ef8fcddc13382239a577fc23
loughrae <loughrae@tcd.ie>
2021-05-06 17:40:45 +0100

    Added biotype and removed peptide_id since I can now get protein_coding directly (hopefully). Classified biotype into Coding, Pseudogene, RNA and Other (things like VDJ Igs and TECs).

--- geneset.R
+++ geneset.R
@@ -8,11 +8,10 @@ geneset <- function(hg) {
   if (hg == 'hg19') {
     ensembl = useEnsembl(biomart = 'genes', dataset = 'hsapiens_gene_ensembl', host = 'grch37.ensembl.org')
   }
-  genes <- getBM(attributes = c('ensembl_gene_id', 'external_gene_name', 'chromosome_name', 'start_position', 'end_position', 'ensembl_peptide_id', 'gene_biotype'), mart = ensembl) %>%
+  genes <- getBM(attributes = c('ensembl_gene_id', 'external_gene_name', 'chromosome_name', 'start_position', 'end_position', 'gene_biotype'), mart = ensembl) %>%
     filter(chromosome_name %in% c(1:22)) %>%
-    mutate(protein = case_when(ensembl_peptide_id != '' ~ 'Coding', TRUE ~ 'Non-Coding')) %>%
-    filter(external_gene_name != '') %>%
-    distinct(external_gene_name, .keep_all = TRUE)
+    mutate(type = case_when(gene_biotype = 'protein_coding' ~ 'Coding', endsWith(gene_biotype, 'pseudogene') ~ 'Pseudogene', endsWith(gene_biotype, 'RNA') ~ 'RNA', TRUE ~ 'Other'))
+    distinct(ensembl_gene_id, .keep_all = TRUE)
   write.table(genes, file = paste0('genes_', hg, '.tsv'), row.names = F, col.names = T, quote = F, sep = '\t')
   
   genes %>% 
