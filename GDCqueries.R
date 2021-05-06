afc3df8e87afbd6a7fd4eee6abb1848bb548fe9d
loughrae <loughrae@tcd.ie>
2021-05-06 18:16:29 +0100

    Download ASCAT data, case metadata and FFPE data.

---
+++ GDCqueries.R
@@ -0,0 +1,28 @@
+library(TCGAbiolinks)
+library(tidyverse)
+
+#### Download allele-specific copy number data #### 
+
+allproj <- getGDCprojects()
+projs <- allproj[startsWith(allproj$id, 'TCGA'),]$id
+dall <- GDCquery(project = projs, data.category = 'Copy Number Variation', workflow.type = 'ASCAT2', data.type = 'Allele-specific Copy Number Segment')
+GDCdownload(dall)
+
+
+#### Case metadata ####
+
+meta <- dall$results[[1]][, c('cases', 'project', 'file_name', 'id')] %>%
+  separate(col = 1, into = c('TCGA', 'TSS', 'Individual', 'Vial', 'Portion', 'Plate', 'Center'), sep = '-', remove = FALSE) %>%
+  mutate(Patient = paste(TCGA, TSS, Individual)) %>%
+  write.table(meta, file = 'GDCquery_results_allcancers.txt', col.names = T, row.names = F, quote = F, sep = '\t')
+
+#### FFPE Clinical Data ####
+
+ffpe <- lapply(projs, FUN = GDCquery_clinic, 'biospecimen') %>% 
+  bind_rows() %>%
+  select(sample_type_id, sample_id, sample_type, submitter_id, is_ffpe) %>%
+  write.table(ffpe, 'ffpe.txt', col.names = T, row.names = F, quote = F, sep= '\t')
+
+
+
+
