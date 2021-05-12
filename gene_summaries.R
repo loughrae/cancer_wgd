9dd7d902525ae629cf992ffb2d018a74a5cb7e7c
loughrae <loughrae@tcd.ie>
2021-05-12 14:52:15 +0100

    Started sample_summaries file to read in individual gene-level copy number files per sample and process them. Currently it reads them in and just prints the mean copy number of all the genes in that sample, and it also filters out genes not on chr1:22, and is most of the way to filtering samples.

--- gene_summaries.R
+++ gene_summaries.R
@@ -7,33 +7,7 @@ none <- mop %>% filter(Count == 0) %>% pull(ensembl_gene_id)
 table(mop$Count == 0)
 table(mop$Mean == -999)
 
-# 
-# mop %>% 
-#     filter(Count > 0) %>%
-#     slice(1:1000) %>% 
-#     group_by(ensembl_gene_id) %>% 
-#     separate_rows(Info, sep = ',') %>% 
-#   data.table %>%
-#   .[,c('CN', 'Cancer', 'WGD') := tstrsplit(Info, "_", fixed = TRUE)] %>%
-#                filter(CN == 4) %>% 
-#                count() %>% 
-#                ggplot(aes(x = n)) + geom_histogram() 
-#   
-#   st = Sys.time()
-#   tro = setDT(trii)[,paste0("Info", 1:3) := tstrsplit(Info, "_", fixed = TRUE)] 
-#   en = Sys.time()
-#   
-#   st2 = Sys.time()
-#   tro = separate(trii, col = Info, into = c('Info1', 'Info2', 'Info3'), sep = '_')
-#   en2 = Sys.time()
-#   
-#   
-#   st3 = Sys.time()
-#   tri1 = mop %>% filter(Count > 0) %>% slice(1:1000) %>% separate_rows(Info, sep = ',') 
-#   tri2 = setDT(tri1)[,c('CN', 'Cancer', 'WGD') := tstrsplit(Info, "_", fixed = TRUE)]
-#   tri3 = tri2 %>% count(ensembl_gene_id, WGD, CN)
-#   en3 = Sys.time()
-#   
+
   mop <- mop %>% filter(Count > 0)
   splitmop <- split(mop , cut(1:nrow(mop), 60))
   process <- function(df) {
