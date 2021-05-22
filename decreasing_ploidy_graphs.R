b9e16d195d91de0a1d940748369ec02dc0adac07
loughrae <loughrae@tcd.ie>
2021-05-22 14:35:21 +0100

    Accepts arguments for ASCAT, ABSOLUTE and PCAWG and works. Also added colour to the boxplots.

--- decreasing_ploidy_graphs.R
+++ decreasing_ploidy_graphs.R
@@ -1,28 +1,44 @@
+args = commandArgs(trailingOnly = TRUE)
+
 library(data.table)
 library(tidyverse)
 library(ggpubr)
 
-absect <- fread('overlaps_absolute_wgd_CN_cut.bed')
-absamples_filtered <- fread('absolute_samples_WGD_filtered.txt')
+long <- fread(args[1])
+sourcename <- args[2]
+samp_means <- fread(args[3])
+
+
+
+if (sourcename %in% c('ascat', 'absolute')) {
+  names(long) <- c('ensembl_gene_id', 'Sample', 'Copy_Number')
+}
+
+if (sourcename == 'ascat') {
+  samp_means$samplename <- as.character(samp_means$samplename)
+}
+
+
 ohnos <- read.table('~/Downloads/strict_ohnologs_full.csv', sep = '\t', header = T) #not actually comma-separated
 oh <- unlist(ohnos)
 uniq_oh <- unique(oh)
 
-absect$ohno <- ifelse(absect$V1 %in% uniq_oh, T, F)
+long$ohno <- ifelse(long$ensembl_gene_id %in% uniq_oh, T, F)
 
 retain <- function(hh, number, source) {
   print(number)
-  df <-  hh %>% group_by(ohno, V2) %>% summarize(total = n(), nums = length(V3[V3 >= number]), perc = length(V3[V3 >= number])/n() , exact = length(V3[V3 == number])/n()) %>% mutate(percent = nums/total) %>%
+  df <-  hh %>% group_by(ohno, Sample) %>% summarize(total = n(), nums = length(Copy_Number[Copy_Number >= number]), perc = length(Copy_Number[Copy_Number >= number])/n() , exact = length(Copy_Number[Copy_Number == number])/n()) %>% mutate(percent = nums/total) %>%
     mutate(Ohnolog = ifelse(ohno == T, 'Ohnolog', 'Non-Ohnolog')) %>%
-    left_join(absamples_filtered, by = c('V2' = 'array')) 
+    left_join(samp_means, by = c('Sample' = 'samplename')) #pcawg already has ploidy col
   ggplot(df, aes(x = ploidy, y = percent, colour = Ohnolog)) + geom_point() + facet_wrap(~Ohnolog) + scale_x_reverse() + xlab('Sample mean copy number') + ylab(paste0('Proportion of genes in at least ', number, ' copies')) + theme(legend.position = 'none')
   ggsave(paste0('decreasing_ploidy_atleast_', number, '_', source, '.png'), width = 20, height = 15)
   ggplot(df, aes(x = ploidy, y = exact, colour = Ohnolog)) + geom_point() + facet_wrap(~Ohnolog) + scale_x_reverse() + xlab('Sample mean copy number') + ylab(paste0('Proportion of genes in exactly ', number, ' copies')) + theme(legend.position = 'none')
   ggsave(paste0('decreasing_ploidy_exactly_', number, '_', source, '.png'), width = 20, height = 15)
-  ggplot(df, aes(x = as.factor(Ohnolog), y = percent)) + geom_boxplot() + stat_compare_means() + xlab('Gene Type') + ylab(paste0('Proportion of genes in at least ', number, ' copies'))
+  ggplot(df, aes(x = as.factor(Ohnolog), y = percent, fill = as.factor(Ohnolog))) + geom_boxplot() + stat_compare_means() + xlab('Gene Type') + ylab(paste0('Proportion of genes in at least ', number, ' copies')) + theme(legend.position = 'none')
   ggsave(paste0('boxplot_atleast_', number, '_', source, '.png'))
   return(df)
 }
 
-retained_3 <- retain(absect, 3, 'absolute')
-retained_4 <- retain(absect, 4, 'absolute')
+retained_3 <- retain(long, 3, sourcename)
+retained_4 <- retain(long, 4, sourcename)
+
